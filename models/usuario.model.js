7334106b04076d21bce660bf61f46c090ef2c8f9
Lucas Omar Moreno <ing.lucasmoreno@gmail.com>
2021-02-10 00:32:10 -0300

    Inicio del proyecto

---
+++ models/usuario.model.js
@@ -0,0 +1,57 @@
+const { Schema, model } = require('mongoose');
+
+const usuarioSchema = Schema({
+    
+    dni: {  // Usuario del sistema
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+
+    apellido: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    nombre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+
+    role: {
+        type: String,
+        required: true,
+        default: 'USER_ROLE'
+    },
+
+    activo: {
+        type: Boolean,
+        required: true,
+        default: true
+    }
+
+}, { timestamps: true });
+
+usuarioSchema.method('toJSON', function(){
+    const {__v, _id, password, ...object} = this.toObject();
+    object.uid = _id;
+    return object;
+});
+
+module.exports = model('usuario', usuarioSchema);

032419580b0f98583a7ff1d7a2fa10a3576f8bff
Lucas Omar Moreno <ing.lucasmoreno@gmail.com>
2021-04-28 23:50:04 -0300

    28/04/2021 - Version 1.1.0

--- database/auto-backup.js
+++ database/auto-backup.js
@@ -12,7 +12,7 @@ const DB_NAME = "edificarSRL";
 const ARCHIVE_PATH = path.join(__dirname, '../backups', `${moment().format('DD-MM-YYYY')}.gzip`);
 
 // Cron - Se ejecuta todos los dias a las 23:59 (Recordar - Debe tener 6 valores)
-cron.schedule('00 23 * * * *',() => backupMongoDB());
+cron.schedule('00 23 * * *',() => backupMongoDB());
 
 // Backup MongoDB
 function backupMongoDB() {
